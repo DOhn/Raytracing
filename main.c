8daec9243dbdbe17cbd1139c491967cc6994f05f
Daniel Ohn <dao48@nau.edu>
2016-12-08 18:43:07 -0700

    Add various functions and changed main to be raycast recursively.

--- main.c
+++ main.c
@@ -13,33 +13,245 @@ float clamp(float v) {
 }
 
 // Creates the output.ppm file.
-void ppmMaker(Color *buff, int x, int y, FILE *output) {
-  fprintf(output, "P6\n");
-  fprintf(output, "%d %d\n", x, y);
-  fprintf(output, "255\n");
-
+void ppmMaker(Color** buff, int x, int y, FILE *output) {
+  fprintf(output, "P%i\n%i %i\n%i\n", 6, x, y, 255);
   int i;
-  for (i = 0; i < x * y; i++) {
-    fputc(buff[i].r, output);
-    fputc(buff[i].g, output);
-    fputc(buff[i].b, output);
+  for(i = 0; i < (x*y); i++) {
+    Color* buffer = buff[i];
+    fprintf(output, "%i\n%i\n%i\n", buffer->r, buffer->g, buffer->b);
   }
 }
 
+void currentIntersect(double* intersect, double* Ro, double* Rd, double t) {
+  intersect[0] = t*Rd[0] + Ro[0];
+  intersect[1] = t*Rd[1] + Ro[1];
+  intersect[2] = t*Rd[2] + Ro[2];
+}
+
+Obj* rayCast(double* t, Obj** objs, Obj* skip, double* Ro, double* Rd) {
+
+  double tNew = INFINITY, tVal;
+  Obj* newObj = NULL;
+  int i;
+
+  for(i = 0; objs[i] != NULL; i++) {
+    if(objs[i] == skip) {
+      continue;
+    } else if(objs[i]->type == 2) {
+      tVal = plane_intersection(Ro, Rd, objs[i]);
+    } else if(objs[i]->type == 1) {
+      tVal = sphere_intersection(Ro, Rd, objs[i]);
+    } else if(objs[i]->type == 0 || objs[i]->type == 3) {
+      continue;
+    } else {
+      fprintf(stderr, "Error: Encountered an Unknown Type.\n");
+      exit(1);
+    }
+
+    if(tVal < tNew && tVal != -1) {
+     tNew = tVal;
+     newObj = objs[i];
+    }
+  }
+    if(tNew == INFINITY) {
+      tNew = -1;
+    }
+   *t = tNew;
+    return newObj;
+}
+
+void reflection(double** reflectColor, double* reflectObjNorm, int dp, double* Ro, double* Rd,
+  Obj** objs, Obj** light, double t) {
+    double reflectObj[3];
+    double tempRo[3];
+
+    currentIntersect(tempRo, Ro, Rd, t - 0.00001);
+    reflection_vector(Rd, reflectObjNorm, reflectObj);
+    *reflectColor = renderColor(dp - 1, tempRo, reflectObj, objs, light);
+
+}
+
+void refraction(double ior, double** refractColor, double* refractNorm, Obj* check, int dp,
+  double* Ro, double* Rd, Obj** objs,  Obj** light, double t) {
+
+  // Used to store new values of Ro, Rd, and t
+  double tempRo[3] = {0};
+  double tempRd[3] = {0};
+  double tNew;
+
+  currentIntersect(tempRo, Ro, Rd, t + 0.00001);
+  refraction_vector(Rd, refractNorm, tempRd, ior);
+
+  if(check->type == 2) {
+    tNew = plane_intersection(tempRo, tempRd, check);
+  } else if(check->type == 1) {
+    tNew = sphere_intersection(tempRo, tempRd, check);
+  } else {
+    fprintf(stderr, "Error: Something went wrong with Refraction.\n");
+    exit(1);
+  }
+
+  if(tNew != -1 && tNew != INFINITY) {
+    currentIntersect(tempRo, tempRo, tempRd, tNew + 0.00001);
+    v3_subtract(check->Sphere.position, tempRo, refractNorm);
+    normalize(refractNorm);
+    refraction_vector(tempRd, refractNorm, tempRd, (1.0 / ior));
+  }
+
+  *refractColor = renderColor(dp - 1, tempRo, tempRd, objs, light);
+}
+
+double* renderColor(int dp, double* Ro, double* Rd, Obj** objs, Obj** light) {
+  double t = 0;
+
+  // Initializing variables and color array
+  double* col = malloc(sizeof(double)*3);
+  col[0] = 0;
+  col[1] = 0;
+  col[2] = 0;
+
+  Obj* obj = rayCast(&t, objs, NULL, Ro, Rd);
+
+  if(t == -1) {
+    return col;
+    }
+  double intersect[3] = {0, 0, 0};
+  double Norm[3] = {0, 0 ,0};
+  currentIntersect(intersect, Ro, Rd, t);
+
+  if(obj->type == 1) {
+    get_sphere_normal(obj, intersect, Norm);
+  } else if(obj->type == 2) {
+    get_plane_normal(obj, Norm);
+  }
+
+  // Setting up the lighting for diffuse and specular
+  double diffColor[3] = {0, 0, 0};
+  double totalDiff[3] = {0, 0, 0};
+  double specColor[3] = {0, 0, 0};
+  double totalSpec[3] = {0, 0, 0};
+  int i;
+
+  for(i = 0; light[i] !=  NULL; i++) {
+    double lightDirect[3] = {0, 0, 0};
+    double lightColor[3] = {0, 0, 0};
+    double lightObj[3] = {0, 0, 0};
+
+    v3_subtract(light[i]->Light.position, intersect, lightDirect);
+    double mag = sqrt(sqr(lightDirect[0]) + sqr(lightDirect[1]) + sqr(lightDirect[2]));
+    normalize(lightDirect);
+
+    // Testing the shadows
+    Obj* temp = rayCast(&t, objs, obj, intersect, lightDirect);
+
+    if(t >= 0 && t < mag && temp->type != 2) {
+      continue;
+    }
+    v3_scale(lightDirect, -1, lightObj);
+    if(light[i]->Light.direct != NULL) {
+      double dot = v3_dot(lightObj, light[i]->Light.direct);
+      if((dot) < sin(light[i]->Light.theta*M_PI/180)) {
+        continue;
+      }
+      v3_scale(lightColor, pow(dot, light[i]->Light.radial_a0), lightColor);
+    }
+    // Less light as it is farther away
+    double radA = 1/(sqr(mag)*(light[i]->Light.radial_a2) + (light[i]->Light.radial_a1)*mag + light[i]->Light.radial_a0);
+    v3_scale(light[i]->diffuse, radA, lightColor);
+
+    // Getting the diffuse color
+    diffuse(diffColor, Norm, lightDirect, obj, lightColor);
+    v3_add(diffColor, totalDiff, totalDiff);
+
+    // Getting the specular color
+    specular(specColor, Norm, lightDirect, obj, lightColor, Rd);
+    v3_add(specColor, totalSpec, totalSpec);
+
+  }
+
+  v3_add(col, totalDiff, col);
+  v3_add(col, totalSpec, col);
+
+
+  if(dp <= 0) {
+    return col;
+  }
+
+  double refractivity = obj->refractivity;
+  double reflectivity = obj->reflectivity;
+  double refracIndex = obj->refracIndex;
+  double* newCol;
+
+  v3_scale(col, 1.0 - (reflectivity + refractivity), col);
+
+
+
+  // Getting the reflection
+  reflection(&newCol, Norm, dp, Ro, Rd, objs, light, t);
+  v3_scale(newCol, reflectivity, newCol);
+  v3_add(col, newCol, col);
+
+  // Getting the refraction
+  refraction(refracIndex, &newCol, Norm, obj, dp, Ro, Rd, objs, light, t);
+  v3_scale(newCol, refractivity, newCol);
+  v3_add(col, newCol, col);
+
+  return col;
+}
+
+Color** sceneMaker(Obj** objs, Obj** light, int height, int width) {
+  // Coordinates of the camera
+  double cx = 0;
+  double cy = 0;
+
+  // Getting the color width and height
+  double h = objs[0]->Camera.height;
+  double w = objs[0]->Camera.width;
+  double* col;
+
+  int M = height;
+  int N = width;
+
+  Color** buff = malloc(M * N * sizeof(Color*));
+
+  double imgH = h / M;
+  double imgW = w / N;
+
+
+  int y, x;
+  for (y = 0; y < M; y++) {
+    for (x = 0; x < N; x++) {
+      double Ro[3] = {0, 0, 0};
+      double Rd[3] = {cx - (w / 2) + imgW * (x + 0.5),
+                      -(cy - (h / 2) + imgH * (y + 0.5)), 1};
+      normalize(Rd);
+      col = renderColor(7, Ro, Rd, objs, light);
+
+      // Setting the color and getting it's values
+      Color* color = malloc(sizeof(Color));
+      color->r = (unsigned char)clamp(col[0]*255);
+      color->g = (unsigned char)clamp(col[1]*255);
+      color->b = (unsigned char)clamp(col[2]*255);
+      buff[y*N + x] = color;
+    }
+  }
+  return buff;
+}
+
 int main(int argc, char *argv[]) {
-  
+
   // Error checking the proper amount of arguments
   if (argc < 5) {
     fprintf(stderr, "Error: Need 4 arguments for this project.\n");
     exit(1);
   }
-  
+
   // Getting height and width values from the arguments.
   int imgW = strtol(argv[1], (char **)NULL, 10);
   int imgH = strtol(argv[2], (char **)NULL, 10);
-  
+
   char *fput = argv[3];
-  
+
   // Error checking for the output file.
   FILE *output = fopen(argv[4], "wb");
   if (!output) {
@@ -47,179 +259,15 @@ int main(int argc, char *argv[]) {
     fclose(output);
     return -1;
   }
-  
-  // Declaring color objects
-  Color color = {.r = 0, .g = 0, .b = 0};
-  Color bgColor = {.r = 0, .g = 0, .b = 0};
-  
-  // Obj pointers used to iterate through the input file.
-  Obj **objs;
-  objs = read_scene(fput);
-  
-  double h = 2;
-  double w = 2;
 
-  int cams = 0;
-  int objNum = 64;
-  int i, j, x, y, k;
-  
-  // Getting the camera values
-  for (i = 0; objs[i] != 0; i++) {
-    if (objs[i]->type == 0) {
-      w = objs[i]->Camera.width;
-      h = objs[i]->Camera.height;
-      cams++;
-    }
-  }
-  
-  // Error checking for amount of cameras
-  if (cams != 1) {
-    fprintf(stderr, "Error: There are too many cameras within the input.json file. There are %d cameras.\n", cams);
-  }
-  
-  // Getting the values of the lights
-  size_t lightNum = 0;
-  Obj **lightArray = malloc(objNum*sizeof(Obj));
-  for (i = 0; objs[i] != 0; i++) {
-    if (objs[i]->type == 3) {
-      lightArray[lightNum] = objs[i];
-      lightNum++;
-    }
-  }
-  lightArray[lightNum] = NULL;
-  
-  // Declaring variables
-  double cx = 0;
-  double cy = 0;
-  int N = imgW;
-  int M = imgH;
-  
-  // Creating the image buffer.
-  Color *buffer = malloc(M * N * sizeof(Color));
-  
-  double pixheight = h / M;
-  double pixwidth = w / N;
-  
-  // Begins to process the image
-  for (y = 0; y < M; y += 1) {
-    for (x = 0; x < N; x += 1) {
-      double Ro[3] = {0, 0, 0};
-      double Rd[3] = {cx - (w / 2) + pixwidth * (x + 0.5),
-        -(cy - (h / 2) + pixheight * (y + 0.5)), 1};
-      normalize(Rd);
-      
-      double closest_t = INFINITY;
-      size_t closest_obj = 0;
-      
-      // Getting value of closest distance
-      for (i = 0; objs[i] != 0; i++) {
-        double t = intersection_dist(Ro, Rd, objs[i]);
-        if (t > 0 && t < closest_t) {
-          closest_t = t;
-          closest_obj = i;
-        }
-      }
-      Obj* closest_object = objs[closest_obj];
-      
-      // Getting vector from surface objects
-      for (j=0; j < lightNum; j+=1) {
-        // Origin of the vector
-        V3 Lo = malloc(3*sizeof(double));
-        closest_t = intersection_dist(Ro, Rd, closest_object);
-        v3_scale(Rd, closest_t-0.1, Lo);
-        
-        // Point to the surface from the light position
-        V3 Ld = malloc(3*sizeof(double));
-        v3_subtract(lightArray[j]->Light.position, Lo, Ld);
-        
-        // Begins working on the shading and shadows
-        double lightDist = v3_magnitude(Ld);
-        Obj* closetShadow = NULL;
-        double closetShadow_t = INFINITY;
-        
-        for (k = 0; objs[k] != 0; k++) {
-          double t = intersection_dist(Lo, Ld, objs[k]);
-          if (t > 0 && t < closest_t) {
-            closetShadow_t = t;
-            closetShadow = objs[k];
-          }
-        }
-        
-        // Begins to compute colors for the shadows
-        if (closetShadow == NULL) {
-          normalize(Rd);
-          normalize(Ld);
+  Color** buff;
+  Obj** objs;
+  Obj** light = malloc(sizeof(Obj*)*128);
 
-          Color diffuse, specular;
-
-          // Getting diffuse value of Sphere/Plane
-          if (closest_object->type == 1) {
-            diffuse = closest_object->Sphere.diffuse;
-          }
-          else {
-            diffuse = closest_object->Plane.diffuse;
-          }
-
-          // Getting specular value of Sphere/Plane
-          if (closest_object->type == 1) {
-            specular = closest_object->Sphere.specular;
-          }
-          else {
-            specular = closest_object->Plane.specular;
-          }
-
-          // Surface normal value calculated
-          V3 normal;
-          if (closest_object->type == 2) {
-            normal = closest_object->Plane.normal;
-          }
-          else {
-            normal = malloc(3*sizeof(double));
-            v3_subtract(Lo, closest_object->Sphere.position, normal);
-          }
-          
-          normalize(normal);
-          // Vector aims towards lightArray.
-          V3 L = Ld;
-          
-          // Reflecting value from the normal
-          V3 R = malloc(3*sizeof(double));
-          v3_scale(normal, 2*v3_dot(L, normal), R);
-          v3_subtract(L, R, R);
-          normalize(R);
-          
-          // Getting the angle values and computing them.
-          double diffCoeff = v3_dot(L, normal);
-          // specCoeff acts as ns variable
-          double specCoeff = pow(v3_dot(R, Rd), 60);
-          double v1 = .125;
-          double v2 = .125;
-          double v3 = .125;
-          
-          color.r = (diffCoeff*diffuse.r*lightArray[j]->Light.color.r) + specCoeff*specular.r*lightArray[j]->Light.color.r;
-          color.g = (diffCoeff*diffuse.g*lightArray[j]->Light.color.g) + specCoeff*specular.g*lightArray[j]->Light.color.g;
-          color.b = (diffCoeff*diffuse.b*lightArray[j]->Light.color.b) + specCoeff*specular.b*lightArray[j]->Light.color.b;
-          
-          if(lightDist < 0 && lightDist >= INFINITY){
-            lightDist = 1;
-          }
-
-          color.r /= (pow(v1, 2)*lightDist + v2*lightDist+v3);
-          color.g /= (pow(v1, 2)*lightDist + v2*lightDist+v3);
-          color.b /= (pow(v1, 2)*lightDist + v2*lightDist+v3);
-        }
-        else{
-          color = bgColor;
-        }
-      }
-      // Getting final color within the buffer
-        buffer[(y*N)+x].r = (uint8_t)(255*clamp(color.r));
-        buffer[(y*N)+x].g = (uint8_t)(255*clamp(color.g));
-        buffer[(y*N)+x].b = (uint8_t)(255*clamp(color.b));
-    }
-  } 
-
+  objs = read_scene(argv[3], light);
+  buff = sceneMaker(objs, light, imgH, imgW);
+  printf("We made it here.\n");
   // Creates the PPM picture in the output file
-  ppmMaker(buffer, N, M, output);
+  ppmMaker(buff, imgH, imgW, output);
   return 0;
 }
