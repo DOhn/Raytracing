f504dc64b7afbc83b36f45007178d1d19c489561
Daniel Ohn <dao48@nau.edu>
2016-12-08 18:44:35 -0700

    Add new variables to parse.

--- parser.c
+++ parser.c
@@ -2,7 +2,6 @@
 
 int line = 1;
 
-
 // next_c() wraps the getc() function and provides error checking and line
 // number maintenance
 
@@ -104,303 +103,201 @@ double *next_vector(FILE *fput) {
  *
  * Returns: Array of Object types
  */
-Obj **read_scene(char *filename) {
-  int c;
-  int objNum = 64;
-  FILE *fput = fopen(filename, "r");
-  if (fput == NULL) {
-    fprintf(stderr, "Error: Could not open file \"%s\"\n", filename);
-    exit(1);
-  }
+ Obj** read_scene(char* filename, Obj** light) {
+   int lightIndex = 0;
+   int index = 0;
+   int c;
+   FILE* json = fopen(filename, "r");
 
-  Obj **objs;
-  objs = malloc(sizeof(Obj *) * objNum);
+   if (json == NULL) {
+     fprintf(stderr, "Error: Could not open file \"%s\"\n", filename);
+     exit(1);
+   }
+   Obj** objs = malloc(sizeof(Obj*)*128);
 
-  skip_ws(fput);
+   objs[127] = NULL;
+   light[127] = NULL;
+   Obj* obj;
 
-  // Getting the beginning of the list to parse
-  expect_c(fput, '[');
-  skip_ws(fput);
+   skip_ws(json);
 
-  size_t currentObj = 0;
-  while (1) {
-    objs[currentObj] = malloc(sizeof(Obj));
+   // Find the beginning of the list
+   expect_c(json, '[');
 
-    c = fgetc(fput);
-    if (c == ']') {
-      fprintf(stderr, "Error: This is the worst scene file EVER.\n");
-      fclose(fput);
-      return NULL;
-    }
-    if (c == '{') {
-      skip_ws(fput);
+   skip_ws(json);
 
-      // Parse the object
-      char *key = next_string(fput);
-      if (strcmp(key, "type") != 0) {
-        fprintf(stderr, "Error: Expected \"type\" key on line number %d.\n",
-                line);
-        exit(1);
-      }
+   // Find the objects
 
-      skip_ws(fput);
-      expect_c(fput, ':');
-      skip_ws(fput);
+   while (1) {
+     c = fgetc(json);
+     if (c == ']') {
+       fprintf(stderr, "Error: This is the worst scene file EVER.\n");
+       fclose(json);
+       return NULL;
+     }
+     if (c == '{') {
+       skip_ws(json);
 
-      char *value = next_string(fput);
+       // Parse the obj
+       char* key = next_string(json);
+       if (strcmp(key, "type") != 0) {
+ 	fprintf(stderr, "Error: Expected \"type\" key on line number %d.\n", line);
+ 	exit(1);
+       }
 
-      if (strcmp(value, "camera") == 0) {
-        objs[currentObj]->type = 0;
-      } else if (strcmp(value, "sphere") == 0) {
-        objs[currentObj]->type = 1;
-      } else if (strcmp(value, "plane") == 0) {
-        objs[currentObj]->type = 2;
-      } else if (strcmp(value, "light") == 0) {
-        objs[currentObj]->type = 3;
-      } else {
-        fprintf(stderr, "Error: Unknown type, \"%s\", on line number %d.\n",
-                value, line);
-        exit(1);
-      }
+       skip_ws(json);
 
-      skip_ws(fput);
+       expect_c(json, ':');
 
-      while (1) {
-        c = next_c(fput);
-        if (c == '}') {
-          // stop parsing this object
-          break;
-        } else if (c == ',') {
-          // read another field
-          skip_ws(fput);
-          char *key = next_string(fput);
-          skip_ws(fput);
-          expect_c(fput, ':');
-          skip_ws(fput);
+       skip_ws(json);
 
-          // Start storing values
+       char* value = next_string(json);
 
-          // Scaler types
-          if (strcmp(key, "width") == 0) {
-            double value = next_number(fput);
-            if (value < 0) {
-              fprintf(stderr, "Error: Width cannot be less than 0. Found %lf "
-                      "on line number %d.\n",
-                      value, line);
-              exit(1);
-            }
+       obj = malloc(sizeof(Obj));
+       obj->refractivity = 0;
+       obj->reflectivity = 0;
+       obj->refracIndex = 1;
 
-            objs[currentObj]->Camera.width = value;
+       if (strcmp(value, "camera") == 0) {
+         obj->type = 0;
+         objs[index++] = obj;
+       } else if (strcmp(value, "sphere") == 0) {
+         obj->type = 1;
+         objs[index++] = obj;
+       } else if (strcmp(value, "plane") == 0) {
+         obj->type = 2;
+         objs[index++] = obj;
+       } else if(strcmp(value, "light") == 0) {
+         obj->type = 3;
+         obj->Light.direct = NULL;
+         light[lightIndex++] = obj;
+       }
+         else {
+ 	       fprintf(stderr, "Error: Unknown type, \"%s\", on line number %d.\n", value, line);
+ 	       exit(1);
+       }
 
-          } else if (strcmp(key, "height") == 0) {
-            double value = next_number(fput);
-            if (value < 0) {
-              fprintf(stderr, "Error: Height cannot be less than 0. Found %lf "
-                      "on line number %d.\n",
-                      value, line);
-              exit(1);
-            }
+       skip_ws(json);
 
-            objs[currentObj]->Camera.height = value;
+       while (1) {
+ 	// , }
+ 	c = next_c(json);
+ 	if (c == '}') {
+ 	  // stop parsing this obj
+ 	  break;
 
-          } else if (strcmp(key, "radius") == 0) {
-            double value = next_number(fput);
-            if (value < 0) {
-              fprintf(stderr, "Error: Radius cannot be less than 0. Found %lf "
-                      "on line number %d.\n",
-                      value, line);
-              exit(1);
-            }
+ 	} else if (c == ',') {
+ 	  // read another field
+ 	  skip_ws(json);
+ 	  char* key = next_string(json);
+ 	  skip_ws(json);
+ 	  expect_c(json, ':');
+ 	  skip_ws(json);
 
-            objs[currentObj]->Sphere.radius = value;
+ 	  if ((strcmp(key, "width") == 0) ||
+ 	      (strcmp(key, "height") == 0) ||
+ 	      (strcmp(key, "radius") == 0) ||
+         (strcmp(key, "theta") == 0) ||
+         (strcmp(key, "radial-a2") == 0) ||
+         (strcmp(key, "radial-a1") == 0) ||
+         (strcmp(key, "radial-a0") == 0) ||
+         (strcmp(key, "angular_a0") == 0)||
+         (strcmp(key, "refractivity") == 0) ||
+         (strcmp(key, "reflectivity") == 0) ||
+         (strcmp(key, "ior") == 0)) {
+ 	         double value = next_number(json);
 
-          } else if (strcmp(key, "theta") == 0) {
-            double value = next_number(fput);
-            value = fmod(value, (2.0 * M_PI));
-            objs[currentObj]->Light.theta = value;
+           if(strcmp(key, "width") == 0) {
+             obj->Camera.width = value;
 
-          } else if (strcmp(key, "angular-a0") == 0) {
-            double value = next_number(fput);
-            if (value < 0) {
-              fprintf(stderr, "Error: Angular-a0 cannot be less than 0. Found %lf "
-                      "on line number %d.\n",
-                      value, line);
-              exit(1);
-            }
+         } else if(strcmp(key, "height") == 0) {
+             obj->Camera.height = value;
 
+         } else if(strcmp(key, "reflectivity") == 0) {
+             obj->reflectivity = value;
 
-            objs[currentObj]->Light.angular_a0 = value;
+         } else if(strcmp(key, "refractivity") == 0) {
+             obj->refractivity = value;
 
-          } else if (strcmp(key, "radial-a0") == 0) {
-            double value = next_number(fput);
-            if (value < 0) {
-              fprintf(stderr, "Error: Radial-a0 cannot be less than 0. Found %lf "
-                      "on line number %d.\n",
-                      value, line);
-              exit(1);
-            }
+         } else if(strcmp(key, "ior") == 0) {
+             obj->refracIndex = value;
 
+         } else if(strcmp(key, "radius") == 0) {
+             obj->Sphere.radius = value;
 
-            objs[currentObj]->Light.angular_a0 = value;
+         } else if(strcmp(key, "theta") == 0) {
+             obj->Light.theta = value;
 
-          } else if (strcmp(key, "radial-a1") == 0) {
-            double value = next_number(fput);
-            if (value < 0) {
-              fprintf(stderr, "Error: Radial-a1 cannot be less than 0. Found %lf "
-                      "on line number %d.\n",
-                      value, line);
-              exit(1);
-            }
+         }  else if(strcmp(key, "radial-a2") == 0) {
+             obj->Light.radial_a2 = value;
 
-            objs[currentObj]->Light.radial_a1 = value;
+         } else if(strcmp(key, "radial-a1") == 0) {
+             obj->Light.radial_a1 = value;
 
-          } else if (strcmp(key, "radial-a2") == 0) {
-            double value = next_number(fput);
-            if (value < 0) {
-              fprintf(stderr, "Error: Radial-a2 cannot be less than 0. Found %lf "
-                      "on line number %d.\n",
-                      value, line);
-              exit(1);
-            }
+         } else if(strcmp(key, "radial-a0") == 0) {
+             obj->Light.radial_a0 = value;
 
-            objs[currentObj]->Light.radial_a2 = value;
+         } else if(strcmp(key, "angular_a0") == 0) {
+             obj->Light.angular_a0 = value;
+         }
+ 	  } else if ((strcmp(key, "diffuse_color") == 0) ||
+ 		     (strcmp(key, "position") == 0) ||
+ 		     (strcmp(key, "normal") == 0) ||
+          (strcmp(key, "color") == 0) ||
+          (strcmp(key, "specular_color") == 0) ||
+          (strcmp(key, "direction") == 0)) {
+           double* value = next_vector(json);
 
-          // Reflectivity, Refraction, and ior
-          } else if (strcmp(key, "reflectivity") == 0) {
-            double value = next_number(fput);
-            if (value > 1 || value < 0) {
-              fprintf(stderr, "Error: Reflectivity must be between 0 - 1."
-                " Currently it is %.2f on line %d\n.", value, line);
-                exit(1);
-            }
-            objs[currentObj]->Sphere.reflect = value;
-          } else if (strcmp(key, "refractivity") == 0) {
-            double value = next_number(fput);
-            if (value > 1 || value < 0) {
-              fprintf(stderr, "Error: Refactivity must be between 0 - 1."
-                " Currently it is %.2f on line %d\n.", value, line);
-                exit(1);
-            }
-            objs[currentObj]->Sphere.refract = value;
-          } else if (strcmp(key, "ior") == 0) {
-            double value = next_number(fput);
-            objs[currentObj]->Sphere.ior = value;
-          }
+           if(strcmp(key, "diffuse_color") == 0) {
+             obj->diffuse = value;
 
-          // Vector types
-          else if (strcmp(key, "color") == 0) {
-            double *value = next_vector(fput);
-            if (value[0] < 0 || value[1] < 0 || value[2] < 0) {
-              fprintf(stderr, "Error: color values cannot be less than 0. "
-                      "On line number %d.\n",
-                      line);
-              exit(1);
-            }
+         } else if(strcmp(key, "direction") == 0) {
+             //printf("Got direct damn\n");
+             obj->Light.direct = value;
 
-            if (objs[currentObj]->type != 3) {
-              fprintf(stderr, "Error: Object does not support a plain color attribute. "
-                      "On line number %d.\n",
-                      line);
-              exit(1);
-            }
+         } else if(strcmp(key, "specular_color") == 0) {
+             obj->specular = value;
 
+         } else if(strcmp(key, "color") == 0) {
+             obj->diffuse = value;
 
-            objs[currentObj]->Light.color.r = value[0];
-            objs[currentObj]->Light.color.g = value[1];
-            objs[currentObj]->Light.color.b = value[2];
+         } else if(strcmp(key, "position") == 0 && obj->type == 2) {
+             obj->Plane.position = value;
 
-          } else if (strcmp(key, "specular_color") == 0) {
-            double *value = next_vector(fput);
+         } else if(strcmp(key, "position") == 0 && obj->type == 1) {
+             obj->Sphere.position = value;
 
-            if (value[0] < 0 || value[1] < 0 || value[2] < 0) {
-              fprintf(stderr, "Error: color values cannot be less than 0. "
-                      "On line number %d.\n",
-                      line);
-              exit(1);
-            }
+         } else if(strcmp(key, "position") == 0 && obj->type == 3) {
+             obj->Light.position = value;
 
-            if (objs[currentObj]->type == 1) {
-              objs[currentObj]->Sphere.specular.r = value[0];
-              objs[currentObj]->Sphere.specular.g = value[1];
-              objs[currentObj]->Sphere.specular.b = value[2];
-            } else if (objs[currentObj]->type == 2) {
-              objs[currentObj]->Plane.specular.r = value[0];
-              objs[currentObj]->Plane.specular.g = value[1];
-              objs[currentObj]->Plane.specular.b = value[2];
-            } else {
-              fprintf(stderr, "Error: Object does not support specular color. "
-                      "On line number %d.\n",
-                      line);
-              exit(1);
-            }
-
-          } else if (strcmp(key, "diffuse_color") == 0) {
-            double *value = next_vector(fput);
-
-            if (value[0] < 0 || value[1] < 0 || value[2] < 0) {
-              fprintf(stderr, "Error: color values cannot be less than 0. "
-                      "On line number %d.\n",
-                      line);
-              exit(1);
-            }
-
-            if (objs[currentObj]->type == 1) {
-              objs[currentObj]->Sphere.diffuse.r = value[0];
-              objs[currentObj]->Sphere.diffuse.g = value[1];
-              objs[currentObj]->Sphere.diffuse.b = value[2];
-            } else if (objs[currentObj]->type == 2) {
-              objs[currentObj]->Plane.diffuse.r = value[0];
-              objs[currentObj]->Plane.diffuse.g = value[1];
-              objs[currentObj]->Plane.diffuse.b = value[2];
-            } else {
-              fprintf(stderr, "Error: Object does not support diffuse color. "
-                      "On line number %d.\n",
-                      line);
-              exit(1);
-            }
-
-          } else if (strcmp(key, "position") == 0) {
-            double *value = next_vector(fput);
-
-            if (objs[currentObj]->type == 2) {
-              v3_cpy(objs[currentObj]->Plane.position, value);
-            } else if (objs[currentObj]->type == 1) {
-              v3_cpy(objs[currentObj]->Sphere.position, value);
-            } else if (objs[currentObj]->type == 3) {
-              v3_cpy(objs[currentObj]->Light.position, value);
-            }
-          } else if (strcmp(key, "normal") == 0) {
-            double *value = next_vector(fput);
-            v3_cpy(objs[currentObj]->Plane.normal, value);
-          } else if (strcmp(key, "direction") == 0) {
-            double *value = next_vector(fput);
-            v3_cpy(objs[currentObj]->Light.dirction, value);
-          } else {
-            fprintf(stderr, "Error: Unknown property, \"%s\", on line %d.\n",
-                    key, line);
-            exit(1);
-          }
-
-          skip_ws(fput);
-        } else {
-          fprintf(stderr, "Error: Unexpected value on line %d\n", line);
-          exit(1);
-        }
-      }
-
-      skip_ws(fput);
-      c = next_c(fput);
-      if (c == ',') {
-        skip_ws(fput);
-      } else if (c == ']') {
-        fclose(fput);
-        objs[currentObj + 1] = 0;
-        return objs;
-      } else {
-        fprintf(stderr, "Error: Expecting ',' or ']' on line %d.\n", line);
-        exit(1);
-      }
-    }
-    currentObj++;
-  }
-}
+         } else if(strcmp(key, "normal") == 0) {
+             obj->Plane.normal = value;
+         }
+ 	  } else {
+ 	    fprintf(stderr, "Error: Unknown property, \"%s\", on line %d.\n",
+ 		    key, line);
+ 	    //char* value = next_string(json);
+ 	  }
+ 	  skip_ws(json);
+ 	} else {
+ 	  fprintf(stderr, "Error: Unexpected value on line %d\n", line);
+ 	  exit(1);
+ 	}
+       }
+       skip_ws(json);
+       c = next_c(json);
+       if (c == ',') {
+ 	// noop
+ 	    skip_ws(json);
+       } else if (c == ']') {
+ 	        fclose(json);
+           objs[index] = NULL;
+           light[lightIndex] = NULL;
+ 	        return objs;
+       } else {
+ 	       fprintf(stderr, "Error: Expecting ',' or ']' on line %d.\n", line);
+ 	       exit(1);
+       }
+     }
+   }
+ }
